0abe8c29ad83d3b6c7199d9e581f76992063a729
razisyahputro <razisyahputro@gmail.com>
2022-07-18 14:18:18 +0700

    serving file statically

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const path = require('path');
-const rootDir = require('../util/path');
+const express = require("express");
+const path = require("path");
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
-  res.redirect('/');
+  res.redirect("/");
 });
 
 module.exports = router;
