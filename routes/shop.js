d54fae5a51c88df9b5954d79c1a1506f3d51970f
razisyahputro <razisyahputro@gmail.com>
2022-08-03 21:11:35 +0700

    adding layout

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ router.get("/", (req, res, next) => {
   console.log(adminData.products);
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
   const products = adminData.products;
-  res.render("shop", { products, title: "Shop" });
+  res.render("shop", { products, title: "Shop", path: "/" });
 });
 
 module.exports = router;
