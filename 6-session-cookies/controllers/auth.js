d8505f99344bf285b9356a46163e6c6219443e03
razisyahputro <razisyahputro@gmail.com>
2024-04-08 16:20:44 +0700

    add cookies in login

--- 6-session-cookies/controllers/auth.js
+++ 6-session-cookies/controllers/auth.js
@@ -1,6 +1,14 @@
 exports.getLogin = (req, res, next) => {
+    const isLoggedIn = !!req.get('Cookie').split(';').find(f => f.trim() === 'loggedIn=true') || false;
+    console.log('isLoggedIn', typeof isLoggedIn)
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'Login'
+        pageTitle: 'Login',
+        isAuthenticated: isLoggedIn
     })
+};
+
+exports.postLogin = (req, res, next) => {
+    res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
+    res.redirect('/');
 };
