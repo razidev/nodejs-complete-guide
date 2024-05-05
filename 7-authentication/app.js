cd915440bdb81dc08ca205584cee1b94af670fa7
razisyahputro <razisyahputro@gmail.com>
2024-05-05 21:49:16 +0700

    remove console & parse

--- 7-authentication/app.js
+++ 7-authentication/app.js
@@ -32,9 +32,8 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  console.log('userx ', req.session)
   if (!req.session.user) return next();
-  const user = JSON.parse(req.session.user);
+  const user = req.session.user;
   User.findById(user._id)
     .then(user => {
       req.user = user;
