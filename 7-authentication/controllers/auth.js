49d1c0ce2f97d712ec21a3f0e1749623bf3dd2e1
razisyahputro <razisyahputro@gmail.com>
2024-05-26 14:41:37 +0700

    remove flag isAuthenticated

--- 7-authentication/controllers/auth.js
+++ 7-authentication/controllers/auth.js
@@ -4,8 +4,7 @@ const User = require('../models/user');
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: false
+    pageTitle: 'Login'
   });
 };
 
@@ -13,7 +12,6 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    isAuthenticated: false
   });
 };
 
