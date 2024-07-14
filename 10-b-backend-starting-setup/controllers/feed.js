04789b6d6fb3bcaa1442621b43aadac4b7b47046
razisyahputro <razisyahputro@gmail.com>
2024-07-14 13:45:40 +0700

    add dummy post request

--- 10-b-backend-starting-setup/controllers/feed.js
+++ 10-b-backend-starting-setup/controllers/feed.js
@@ -1,3 +1,5 @@
+const  { validationResult } =  require('express-validator');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [{
@@ -14,5 +16,26 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
-
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            message: 'Validation failed',
+            errors: errors.array()
+        });
+    }
+    const title = req.body.title;
+    const content = req.body.content;
+    console.log(req.body)
+    res.status(201).json({
+        message: 'Post created successfully',
+        post: {
+            _id: new Date().toDateString(),
+            title: title,
+            content: content,
+            creator: {
+                name: 'John Doe'
+            },
+            createdAt: new Date()
+        }
+    })
 };
