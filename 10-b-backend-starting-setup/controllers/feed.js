21309dff0ed69d7e1169bb73f6c3785c614be6e1
razisyahputro <razisyahputro@gmail.com>
2024-07-23 10:51:46 +0700

    fetching post

--- 10-b-backend-starting-setup/controllers/feed.js
+++ 10-b-backend-starting-setup/controllers/feed.js
@@ -2,18 +2,14 @@ const  { validationResult } =  require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [{
-            _id: '1',
-            title: 'First Post',
-            content: 'This is the content of the first post.',
-            imageUrl: 'images/me.jpg',
-            creator: {
-                name: 'John Doe'
-            },
-            createdAt: new Date()
-        }]
-    })
+    Post.find().then(posts => {
+        res.status(200).json({ posts })
+    }).catch(err =>{
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
@@ -46,4 +42,25 @@ exports.createPost = (req, res, next) => {
         next(err); //  use next() to reach next middleware error, because inside the catch block
     })
     
+};
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+       .then(post => {
+            if (!post) {
+                const error = new Error('Post not found');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({
+                post: post
+            })
+        })
+       .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err); //  use next() to reach next middleware error, because inside the catch block
+        })
 };
--- 10-b-backend-starting-setup/routes/feed.js
+++ 10-b-backend-starting-setup/routes/feed.js
@@ -8,5 +8,6 @@ router.post('/post', [
     body('title').trim().isLength({ min: 7 }),
     body('content').trim().isLength({ min: 5 })
 ], feedController.createPost);
+router.get('/post/:postId', feedController.getPost);
 
 module.exports = router;
