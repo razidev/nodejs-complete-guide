44f0813eb2c65a555e0c2e516915f8eed0b36bf6
razisyahputro <razisyahputro@gmail.com>
2024-08-13 21:50:40 +0700

    add pagination

--- 10-b-backend-starting-setup/controllers/feed.js
+++ 10-b-backend-starting-setup/controllers/feed.js
@@ -4,14 +4,25 @@ const  { validationResult } =  require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    Post.find().then(posts => {
-        res.status(200).json({ posts })
-    }).catch(err =>{
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
+    Post.find().countDocuments()
+    .then(count => {
+        totalItems = count;
+        return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
+    .then(posts => {
+        res.status(200).json({ posts, totalItems });
+    })
+    .catch(err => {
         if (!err.statusCode) {
             err.statusCode = 500;
         }
         next(err);
-    });
+    })
 };
 
 exports.createPost = (req, res, next) => {
