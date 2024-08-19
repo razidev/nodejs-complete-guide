eb87f0d9e5bca91898aade850fd8825ef57a3923
razisyahputro <razisyahputro@gmail.com>
2024-08-19 23:27:01 +0700

    implement authorization

--- 10-b-backend-starting-setup/routes/feed.js
+++ 10-b-backend-starting-setup/routes/feed.js
@@ -5,15 +5,15 @@ const feedController = require('../controllers/feed');
 const isAuth = require('../middleware/is-auth');
 
 router.get('/posts', isAuth, feedController.getPosts);
-router.post('/post', [
+router.post('/post', isAuth, [
     body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 5 })
 ], feedController.createPost);
-router.get('/post/:postId', feedController.getPost);
-router.put('/post/:postId', [
+router.get('/post/:postId', isAuth, feedController.getPost);
+router.put('/post/:postId', isAuth, [
     body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 5 })
 ], feedController.updatePost);
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
