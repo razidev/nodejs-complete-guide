9ef65fb98b4d42d8ce0506c9281917bcd42b9ba7
razisyahputro <razisyahputro@gmail.com>
2024-09-24 12:03:17 +0700

    add api register

--- 11-GraphQL/app.js
+++ 11-GraphQL/app.js
@@ -43,7 +43,8 @@ app.use((req, res, next) => {
 
 app.use('/graphql', graphqlHTTP({
    schema: graphqlSchema,
-   rootValue: graphqlResolver
+   rootValue: graphqlResolver,
+   graphiql: true,
 }));
 
 app.use((error, req, res, next) => {
--- 11-GraphQL/graphql/resolvers.js
+++ 11-GraphQL/graphql/resolvers.js
@@ -1,8 +1,21 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+
 module.exports = {
-    hello() {
+    createUser: async function ({ userInput }, req) {
+        const { email, name, password } = userInput;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            throw new Error('User already exists!');
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const user = new User({ email, name, password: hashedPassword });
+        const result = await user.save();
+
         return {
-            text: 'hello world',
-            views: 1995
+            ...result._doc,
+            _id: result._id.toString(),
         };
     }
 };
