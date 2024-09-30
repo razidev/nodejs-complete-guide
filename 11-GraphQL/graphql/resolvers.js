e2bdf8cad3bd2ac4c8c16463428e331015e05954
razisyahputro <razisyahputro@gmail.com>
2024-09-30 12:44:02 +0700

    add login functionality

--- 11-GraphQL/graphql/resolvers.js
+++ 11-GraphQL/graphql/resolvers.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const validator = require('validator');
 const User = require('../models/user');
 
@@ -32,6 +33,29 @@ module.exports = {
         return {
             ...result._doc,
             _id: result._id.toString(),
+        };
+    },
+    login: async function ({ email, password }) {
+        const user = await User.findOne({ email });
+        if (!user) {
+            const error = new Error('User not found!');
+            error.code = 401;
+            throw error;
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+            const error = new Error('Password is incorrect!');
+            error.code = 401;
+            throw error;
+        }
+        const token = await jwt.sign({
+            userId: user._id.toString(),
+            email: user.email
+        }, 'secretjwtket', { expiresIn: '1h' });
+
+        return {
+            token,
+            userId: user._id.toString(),
         };
     }
 };
