aac3d1e549d07d06c8b41a2513dd7bfa66300b05
razisyahputro <razisyahputro@gmail.com>
2024-10-07 13:26:47 +0700

    implement single post

--- 11-GraphQL/graphql/resolvers.js
+++ 11-GraphQL/graphql/resolvers.js
@@ -133,4 +133,23 @@ module.exports = {
             })
         }
     },
+    post: async function({ id }, req) {
+        if (!req.isAuth) {
+            const error = new Error('Not authenticated!');
+            error.code = 401;
+            throw error;
+        }
+        const post = await Post.findById(id).populate('creator');;
+        if (!post) {
+            const error = new Error('Post not found!');
+            error.statusCode = 404;
+            throw error;
+        }
+        return {
+           ...post._doc,
+            _id: post._id.toString(),
+            createdAt: post.createdAt,
+            updatedAt: post.updatedAt,
+        }
+    }
 };
